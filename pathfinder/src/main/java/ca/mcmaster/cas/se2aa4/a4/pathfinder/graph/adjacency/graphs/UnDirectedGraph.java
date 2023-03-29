0dd5d8390645af7d053eac11f39c76315b576315
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-29 13:57:00 -0400

    F01: Implement undirected graphs

---
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/graph/adjacency/graphs/UnDirectedGraph.java
@@ -0,0 +1,65 @@
+package ca.mcmaster.cas.se2aa4.a4.pathfinder.graph.adjacency.graphs;
+
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.edge.Edge;
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.edge.edges.DefaultEdge;
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.graph.adjacency.AdjacencyGraph;
+
+import java.util.List;
+import java.util.Set;
+
+public class UnDirectedGraph<T> extends AdjacencyGraph<T> {
+
+    public UnDirectedGraph(Class<T> dataClass) {
+        this(dataClass, false);
+    }
+
+    public UnDirectedGraph(Class<T> dataClass, boolean isWeighted) {
+        super(dataClass, DefaultEdge.class, isWeighted);
+    }
+
+    @Override
+    public void addEdge(T t1, T t2) {
+        super.addEdge(t1, t2);
+        super.addEdge(t2, t1);
+    }
+
+    @Override
+    public void addEdge(Edge edge) {
+        T t1 = super.getEdgeSourceNode(edge);
+        T t2 = super.getEdgeTargetNode(edge);
+        this.addEdge(t1, t2);
+    }
+
+    @Override
+    public int getNumEdges() {
+        return super.getNumEdges()/2;
+    }
+
+    @Override
+    protected void specificRemoveEdge(T t1, T t2) {
+        Set<Edge> t1Edges = this.graph.get(t1);
+        Set<Edge> t2Edges = this.graph.get(t2);
+        Edge t1EdgeRemove = new DefaultEdge(t1, t2);
+        Edge t2EdgeRemove = new DefaultEdge(t2, t1);
+        super.weightMap.remove(t1EdgeRemove);
+        super.weightMap.remove(t2EdgeRemove);
+        t1Edges.remove(t1EdgeRemove);
+        t2Edges.remove(t2EdgeRemove);
+    }
+
+    @Override
+    public List<Edge> getNodeEdges(T t) {
+        return super.getEdges().stream()
+                .filter(e -> t.equals(e.getSourceNode()) || t.equals(e.getTargetNode()))
+                .toList();
+    }
+
+    @Override
+    protected boolean checkEdges(T t1, T t2) {
+        Set<Edge> t1Edges = this.graph.get(t1);
+        Set<Edge> t2Edges = this.graph.get(t2);
+        boolean t1Contains = t1Edges.stream().anyMatch(e -> t2.equals(e.getTargetNode()));
+        boolean t2Contains = t2Edges.stream().anyMatch(e -> t1.equals(e.getTargetNode()));
+        return t1Contains && t2Contains;
+    }
+}
