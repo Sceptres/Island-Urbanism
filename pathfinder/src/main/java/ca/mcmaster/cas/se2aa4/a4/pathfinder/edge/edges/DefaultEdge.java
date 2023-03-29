b839c6030602bb98dfbbda90e4dffcbe646cad28
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-29 12:42:51 -0400

    F01: The default edge implementation.

---
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/edge/edges/DefaultEdge.java
@@ -0,0 +1,42 @@
+package ca.mcmaster.cas.se2aa4.a4.pathfinder.edge.edges;
+
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.edge.Edge;
+
+import java.util.Objects;
+
+public class DefaultEdge implements Edge {
+    private final Object source;
+    private final Object target;
+
+    public DefaultEdge(Object source, Object target) {
+        this.source = source;
+        this.target = target;
+    }
+
+    @Override
+    public Object getSourceNode() {
+        return this.source;
+    }
+
+    @Override
+    public Object getTargetNode() {
+        return this.target;
+    }
+
+    @Override
+    public String toString() {
+        return this.source + " -> " + this.target;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DefaultEdge that)) return false;
+        return Objects.equals(source, that.source) && Objects.equals(target, that.target);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, target);
+    }
+}
